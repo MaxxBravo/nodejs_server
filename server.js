758bb2b059541c1af7e822c96ebf7eef751206b4
MaxxBravo <maxxbr.iee@gmail.com>
2019-10-02 22:28:35 -0300

    Ajustes para RRHH

--- server.js
+++ server.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const app = express();
 
+
+app.use('/static', express.static(__dirname + '/public'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended : true}));
+
+app.engine('html',require('ejs').renderFile);
+app.set('view engine','html');
+
 app.get('/', (req, res) => {
-    res.send('<h3>Acá está el Hola Mundo! Sabiondo, esta no es la URL que te pase.</h3>');
+    // res.send('<h3>Acá está el Hola Mundo! Sabiondo, esta no es la URL que te pase.</h3>');
+    res.render('pages/ergo_Phyton.html');
 });
-app.get('/ma', (req, res) => {
-    res.send('<h3>Hola Ma!</h3>');
-});
-app.get('/pa', (req, res) => {
-    res.send('<h3>Hola Pa!</h3>');
-});
-app.get('/macro', (req, res) => {
-    res.send('<h3>Hola Equipo Macro!</h3>');
-});
-app.get('/ergo', (req, res) => {
-    res.send('<h3>Hola Equipo Ergo!</h3>');
-});
+// app.get('/ma', (req, res) => {
+//     res.send('<h3>Hola Ma!</h3>');
+// });
+// app.get('/pa', (req, res) => {
+//     res.send('<h3>Hola Pa!</h3>');
+// });
+// app.get('/macro', (req, res) => {
+//     res.send('<h3>Hola Equipo Macro!</h3>');
+// });
+// app.get('/ergo', (req, res) => {
+//     res.send('<h3>Hola Equipo Ergo!</h3>');
+// });
 
 app.get('*', (req, res) => {
-    res.send('<h3>URL no válida</h3>');
+    // res.send('<h3>URL no válida</h3>');
+    res.redirect('/');
 })
 
 const server = app.listen(process.env.PORT || 8080, () => {
